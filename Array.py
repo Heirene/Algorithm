21686afe70e0e1d60e8c76f891efa8fdd1e01d1b
ihe <irene.he@infor.com>
2019-12-05 11:11:56 +0800

    Add array.py

---
+++ Array.py
@@ -0,0 +1,26 @@
+class Array:
+
+    def __init__(self, capacity: int):
+        self._data = []
+        self._capacity = capacity
+
+    def __getitem__(self, position: int) -> object:
+        return self._data[position]
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __setitem__(self, index: int, value: object):
+        self.data[index] = value
+
+    def insert(self, index: int, value: int) -> bool:
+        if len(self) >= self._capacity:
+            return False
+        else:
+            return self._data.insert(index, value)
+
+if __name__ == "__main__":
+    array = Array(5)
+    array.insert(0,1)
+    print(array[0])
+
